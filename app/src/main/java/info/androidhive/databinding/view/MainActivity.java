10a793ac6cc9e4a77ccc704dea7c5f2c865e6a05
vinay149 <rit2013029@iiita.ac.in>
2018-11-18 22:27:37 +0530

    Initial commit

---
+++ app/src/main/java/info/androidhive/databinding/view/MainActivity.java
@@ -0,0 +1,153 @@
+package info.androidhive.databinding.view;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import info.androidhive.databinding.R;
+import info.androidhive.databinding.databinding.ActivityMainBinding;
+import info.androidhive.databinding.model.Post;
+import info.androidhive.databinding.model.User;
+import info.androidhive.databinding.utils.GridSpacingItemDecoration;
+
+public class MainActivity extends AppCompatActivity implements PostsAdapter.PostsAdapterListener {
+
+    private MyClickHandlers handlers;
+    private PostsAdapter mAdapter;
+    private RecyclerView recyclerView;
+    private ActivityMainBinding binding;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.toolbar_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        handlers = new MyClickHandlers(this);
+
+       // initRecyclerView();
+
+        renderProfile();
+    }
+
+    /**
+     * Renders RecyclerView with Grid Images in 3 columns
+     */
+    private void initRecyclerView() {
+        recyclerView = binding.content.recyclerView;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(3, dpToPx(4), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+        mAdapter = new PostsAdapter(getPosts(), this);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    /**
+     * Renders user profile data
+     */
+    private void renderProfile() {
+        user = new User();
+        user.setName("David Attenborough");
+        user.setEmail("david@natgeo.com");
+        user.setProfileImage("https://api.androidhive.info/images/nature/david.jpg");
+        user.setAbout("Naturalist");
+
+        // ObservableField doesn't have setter method, instead will
+        // be called using set() method
+        user.numberOfPosts.set(3400L);
+        user.numberOfFollowers.set(3050890L);
+        user.numberOfFollowing.set(150L);
+
+
+        // display user
+        binding.setUser(user);
+
+        // assign click handlers
+        binding.content.setHandlers(handlers);
+    }
+
+    private ArrayList<Post> getPosts() {
+        ArrayList<Post> posts = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            Post post = new Post();
+            post.setImageUrl("https://api.androidhive.info/images/nature/" + i + ".jpg");
+
+            posts.add(post);
+        }
+
+        return posts;
+    }
+
+    @Override
+    public void onPostClicked(Post post) {
+        Toast.makeText(getApplicationContext(), "Post clicked! " + post.getImageUrl(), Toast.LENGTH_SHORT).show();
+    }
+
+    public class MyClickHandlers {
+
+        Context context;
+
+        public MyClickHandlers(Context context) {
+            this.context = context;
+        }
+
+        /**
+         * Demonstrating updating bind data
+         * Profile name, number of posts and profile image
+         * will be updated on Fab click
+         */
+        public void onProfileFabClicked(View view) {
+            user.setName("Sir David Attenborough");
+            user.setProfileImage("https://api.androidhive.info/images/nature/david1.jpg");
+
+            // updating ObservableField
+            user.numberOfPosts.set(5500L);
+            user.numberOfFollowers.set(5050890L);
+            user.numberOfFollowing.set(180L);
+        }
+
+        public boolean onProfileImageLongPressed(View view) {
+            Toast.makeText(getApplicationContext(), "Profile image long pressed!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+
+        public void onFollowersClicked(View view) {
+            Toast.makeText(context, "Followers is clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onFollowingClicked(View view) {
+            Toast.makeText(context, "Following is clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onPostsClicked(View view) {
+            Toast.makeText(context, "Posts is clicked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Converting dp to pixe
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+}
---
+++ app/src/main/java/info/androidhive/databinding/view/PostsAdapter.java
@@ -0,0 +1,66 @@
+package info.androidhive.databinding.view;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import info.androidhive.databinding.R;
+import info.androidhive.databinding.databinding.PostRowItemBinding;
+import info.androidhive.databinding.model.Post;
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder> {
+
+    private List<Post> postList;
+    private LayoutInflater layoutInflater;
+    private PostsAdapterListener listener;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private final PostRowItemBinding binding;
+
+        public MyViewHolder(final PostRowItemBinding itemBinding) {
+            super(itemBinding.getRoot());
+            this.binding = itemBinding;
+        }
+    }
+
+
+    public PostsAdapter(List<Post> postList, PostsAdapterListener listener) {
+        this.postList = postList;
+        this.listener = listener;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        PostRowItemBinding binding =
+                DataBindingUtil.inflate(layoutInflater, R.layout.post_row_item, parent, false);
+        return new MyViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        holder.binding.setPost(postList.get(position));
+        holder.binding.thumbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onPostClicked(postList.get(position));
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    public interface PostsAdapterListener {
+        void onPostClicked(Post post);
+    }
+}
